4c8c682fd26fe1c89e5536c74de67a9f7215ba69
vesnaklajic <60219677+vesnaklajic@users.noreply.github.com>
2020-11-07 18:23:20 +0100

    Delete monicaTableEmployee.sql

--- monicaTableEmployee.sql
+++
@@ -1,5 +0,0 @@
-insert into employee values ('Juan','Perez', 'jp123', '123jp', 'juan.p@gmail.com','0145859938', '0688995214');
-insert into employee values ('Angela','Merkel', 'ma5698', '5698ma', 'angela.merkellall@gmail.com', '0154983540', '0798352519');
-insert into employee values ('Jacob','Rousseau', 'jr5620', '5620rj', 'j.rousseau@gmail.com', '0189251818', '0698252519');
-insert into employee values ('Edgar','Poe', 'PE325', '3288ep', 'p.e@gmail.com', '0154983540', '0698252519');
-insert into employee values ('Charles','Perraul', 'Pchar4568', '4568pchar', 'charlesperault@gmail.com', '0185963257', '0658362121');
